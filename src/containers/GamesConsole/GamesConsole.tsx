4e5842d67d3aa18f8e72d4804dc02f481a275448
nadocodes <naderantar96@gmail.com>
2023-04-22 00:54:17 +0100

    config states for games present

--- src/containers/GamesConsole/GamesConsole.tsx
+++ src/containers/GamesConsole/GamesConsole.tsx
@@ -1,12 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './GamesConsole.css';
 
 export default function Games() {
+    const [menu, setMenu] = useState(true);
     const [game, setGame] = useState('');
-    const [games, setGames] = useState(['']);
+
+    useEffect(() => {
+        if (game !== '') {
+            setMenu(false);
+        } else {
+            setMenu(true);
+        }
+    }, [game]);
+
   return (
     <div className='GamesContainer'>
-        <h3>Games</h3>
+        {menu && <div className="menu">
+        <h3 className="title">Games</h3>
+        <p>Select A Game</p>
+          <div className="games">
+              <div className="game">
+                  <button className="gameButton" onClick={() => setGame('RockPaperScissors')}>Rock Paper Scissors</button>
+              </div>
+              <div className="game">
+                  <button className="gameButton" onClick={() => setGame('MemoryGame')}>Memory Game</button>
+              </div>
+              <div className="game">
+                  <button className="gameButton" onClick={() => setGame('')}>Coming Soon!</button>
+              </div>
+              <div className="game">
+                  <button className="gameButton" onClick={() => setGame('')}>Coming Soon!</button>
+              </div>
+          </div> 
+        </div>}
     </div>
   )
 }
