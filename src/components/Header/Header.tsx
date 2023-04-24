f09b241763bb15735370b088fe2c71b4e66b386a
nadocodes <naderantar96@gmail.com>
2023-04-25 00:37:20 +0100

    updated header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,13 @@ function Header(props: { theme: string }) {
   const { theme } = props;
   return (
     <div className="Header">
+      <div className="mainContainer">
       <div className="myInfo">
         <h2>Nader Antar</h2>
         <h1>Web Engineer</h1>
-        <img className="ScriptSvg" src={theme == 'dark' ? ScriptSvgDark : ScriptSvgLight} alt="Script" />
       </div>
+        <img className="ScriptSvg" src={theme == 'dark' ? ScriptSvgDark : ScriptSvgLight} alt="Script" />
+        </div>
         <Games />
     </div>
   )
