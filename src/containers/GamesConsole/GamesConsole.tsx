c6624747fd60025e24a5bffc4f08fb69635b4518
nadocodes <naderantar96@gmail.com>
2023-04-24 01:52:59 +0100

    refractored menu buttons to map for future additions

--- src/containers/GamesConsole/GamesConsole.tsx
+++ src/containers/GamesConsole/GamesConsole.tsx
@@ -6,6 +6,8 @@ import './GamesConsole.css';
 export default function Games() {
     const [menu, setMenu] = useState(true);
     const [game, setGame] = useState('');
+    const [gameName, setGameName] = useState(['Rock Paper Scissors', 'Memory Game']);
+    const [gamesId, setGamesId] = useState(['RockPaperScissors', 'MemoryGame', '', '']);
 
     useEffect(() => {
         if (game) {
@@ -27,21 +29,25 @@ export default function Games() {
                     <h3 className="menuTitle">Mini Games</h3>
                     <p className="menuText">Select A Game</p>
                     <div className="games">
-                        <div className="game">
-                            <button className="gameButton" onClick={() => setGame('RockPaperScissors')}>Rock Paper Scissors</button>
-                        </div>
-                        <div className="game">
-                            <button className="gameButton" onClick={() => setGame('MemoryGame')}>Memory Game</button>
-                        </div>
-                        <div className="game">
-                            <button className="gameButton" onClick={() => setGame('')}>Coming Soon!</button>
-                        </div>
-                        <div className="game">
-                            <button className="gameButton" onClick={() => setGame('')}>Coming Soon!</button>
-                        </div>
+                        {gamesId.map((gameId, idx) => {
+                            if (gameId){
+                            return (
+                                <div className="game">
+                                    <button className="gameButton" onClick={() => setGame(gameId)}>{gameName[idx]}</button>
+                                </div>
+                            )} else {
+                                return (
+                                    <div className="game">
+                                        <button className="gameButton" onClick={() => setGame('')}>Coming Soon!</button>
+                                    </div>
+                                )
+                            }
+                        })
+                        }
+
                     </div>
                 </div>}
-            {game === 'RockPaperScissors' && <div className="gameDisplay"><RockPaperScissors /></div>}
+            {game === 'RockPaperScissors' && <RockPaperScissors />}
             </div>
             <div className="controls">
                 <button className="menuButton" onClick={handleMenu}><AiFillHome /></button>
