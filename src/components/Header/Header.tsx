95e1202f675e96bc1fd6ef9fbabff956df1881ad
nadocodes <naderantar96@gmail.com>
2023-04-25 01:31:26 +0100

    updated header layout and optimised for screen compatability

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,14 +8,14 @@ function Header(props: { theme: string }) {
   const { theme } = props;
   return (
     <div className="Header">
+        <Games />
       <div className="mainContainer">
       <div className="myInfo">
         <h2>Nader Antar</h2>
         <h1>Web Engineer</h1>
       </div>
         <img className="ScriptSvg" src={theme == 'dark' ? ScriptSvgDark : ScriptSvgLight} alt="Script" />
-        </div>
-        <Games />
+      </div>
     </div>
   )
 }
