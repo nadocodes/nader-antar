9a61d5b3e240168bb3098966aa16169aebed7947
nadocodes <naderantar96@gmail.com>
2023-04-28 21:58:33 +0100

    updated projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Projects.css'
 
 function Projects() {
   return (
-    <div className='Projects sectionDefault' id='projects' >Projects</div>
+    <div className='Projects sectionDefault' id='projects'>
+      <h2 className="projectsTitle">Projects</h2>
+      <div className='projectsContainer'>
+        <div className='projectCard'>
+          <h3>Project 1</h3>
+          <p>Project 1 description</p>
+        </div>
+        <div className='projectCard'>
+          <h3>Project 2</h3>
+          <p>Project 2 description</p>
+        </div>
+      </div>
+      </div>
   )
 }
 
