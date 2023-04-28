21170bd40806140c16e39dc56f0de4a1719dbf25
nadocodes <naderantar96@gmail.com>
2023-04-28 19:35:39 +0100

    updated site responsiveness and layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,13 +8,13 @@ function Header(props: { theme: string }) {
   const { theme } = props;
   return (
     <div className="Header sectionDefault" id='home'>
-        <Games />
       <div className="mainContainer">
       <div className="myInfo">
         <h2>Nader Antar</h2>
         <h1>Web Engineer</h1>
       </div>
-        <img className="ScriptSvg" src={theme == 'dark' ? ScriptSvgDark : ScriptSvgLight} alt="Script" />
+        <Games />
+        {/* <img className="ScriptSvg" src={theme == 'dark' ? ScriptSvgDark : ScriptSvgLight} alt="Script" /> */}
       </div>
     </div>
   )
