66459784d5c9d4616cc9816116bd8b36332a7c8e
nadocodes <naderantar96@gmail.com>
2023-05-02 00:47:22 +0100

    ~

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,6 +4,7 @@ import './Contact.css'
 function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,13 +15,37 @@ function Contact() {
     setEmail(e.target.value);
   }
 
+  const handleSubjectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubject(e.target.value);
+  }
+
   const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(name, email, message);
+    console.log(name, email, subject, message);
+    const data = {
+      name,
+      email,
+      subject,
+      message
+    };
+
+    fetch('/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => {
+      if (response.ok) {
+        alert('Your message has been sent.');
+      } else {
+        alert('There was a problem sending your message.');
+      }
+    });
   }
 
   return (
@@ -30,7 +55,9 @@ function Contact() {
         <label htmlFor='name'>Name: </label>
         <input type='text' id='name' name='name' placeholder='Your name...' onChange={handleNameChange} />
         <label htmlFor='email'>Email: </label>
-        <input type='email' id='email' name='email' placeholder='Your email...' onChange={handleEmailChange} />
+        <input type='email' id='email' name='email' placeholder='Your email...' onChange={handleEmailChange}/>
+        <label htmlFor='subject'>Subject: </label>
+        <input type='text' id='subject' name='subject' placeholder='Subject...' onChange={handleSubjectChange}/>
         <label htmlFor='message'>Message: </label>
         <textarea id='message' name='message' placeholder='Your message...' rows={10} cols={60} onChange={handleMessageChange} ></textarea>
         <input type='submit' value='Submit' />
