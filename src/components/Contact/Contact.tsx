90ae08f63e39399573374a21b7f52a68ccd5be9f
nadocodes <naderantar96@gmail.com>
2023-05-02 22:35:53 +0100

    updated contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -61,15 +61,21 @@ function Contact() {
     <div className='Contact secondaryDefault' id='contact'>
       <h2>Contact Me</h2>
       <form className='ContactForm' onSubmit={sendMail}>
-        <label htmlFor='name'>Name: </label>
-        <input type='text' id='name' name='name' placeholder='Your name...' value={name} onChange={handleNameChange} />
-        <label htmlFor='email'>Email: </label>
-        <input type='email' id='email' name='email' placeholder='Your email...' value={email} onChange={handleEmailChange}/>
-        <label htmlFor='subject'>Subject: </label>
-        <input type='text' id='subject' name='subject' placeholder='Subject...' value={subject} onChange={handleSubjectChange}/>
+        <div className='formField'>
+          <label htmlFor='name'>Name: </label>
+          <input type='text' id='name' name='name' placeholder='Your name...' value={name} onChange={handleNameChange} />
+        </div>
+        <div className='formField'>
+          <label htmlFor='email'>Email: </label>
+          <input type='email' id='email' name='email' placeholder='Your email...' value={email} onChange={handleEmailChange}/>
+        </div>
+        <div className='formField'>
+          <label htmlFor='subject'>Subject: </label>
+          <input type='text' id='subject' name='subject' placeholder='Subject...' value={subject} onChange={handleSubjectChange}/>
+        </div>
         <label htmlFor='message'>Message: </label>
         <textarea id='message' name='message' placeholder='Your message...' value={message} rows={10} cols={60} onChange={handleMessageChange} ></textarea>
-        <input type='submit' value='Submit' />
+        <input type='submit' value='Send' className='sendEmail' />
       </form>
       <p>Feel free to reach out to me at <a href='mailto:naderantar96@gmail.com'>NaderAntar96@gmail.com</a></p>
     </div>
