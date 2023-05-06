3ead712b155eb4f5a2e1d49dec08c9c2085d5930
nadocodes <naderantar96@gmail.com>
2023-05-06 02:04:55 +0100

    added links with icons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
 import './Footer.css'
 
 function Footer() {
   return (
     <div className='Footer'>
         <p>© Nader Antar 2023</p>
+        <div className='footerLinks'>
+          <a href='https://github.com/nadocodes' target='_blank' rel='noopener noreferrer'>
+            <FaGithub size={30} />
+          </a>
+          <a href='https://www.linkedin.com/in/nader-antar/' target='_blank' rel='noopener noreferrer'>
+            <FaLinkedin size={30} />
+          </a>
+          <a href='mailto:naderantar96@gmail.com'>
+            <FaEnvelope size={30} />
+          </a>
+        </div>
     </div>
   )
 }
