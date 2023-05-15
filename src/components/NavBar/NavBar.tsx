eef880ebc0d6a46029c08c224c0e811cd16e304d
nadocodes <naderantar96@gmail.com>
2023-05-15 05:42:43 +0100

    updated navbar look by adding secondary bg colorway

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,8 +9,8 @@ function NavBar(props: { toggleTheme: () => void } & { theme: string }){
     <div>
         <nav>
             <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li> |
+                <li><a href="#projects">Projects</a></li> |
                 <li><a href="#contact">Contact</a></li>
             </ul>
             <button className='ThemeButton' onClick={toggleTheme} aria-label='Toggle Theme Button'><img alt='Theme Toggle Icon' src={theme === 'dark' ? LightButton : DarkButton} /></button>
