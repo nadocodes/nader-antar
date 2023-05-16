778021337690765e75163c6db8c6c2ae3c4407bd
nadocodes <naderantar96@gmail.com>
2023-05-16 07:03:39 +0100

    updated active card class

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -26,11 +26,11 @@ function Projects() {
     <div className='Projects primaryDefault' id='projects'>
       <h2 className="projectsTitle">Projects</h2>
       <div className='projectsContainer'>
-        <div className='projectCard activeProjectCard' onClick={handleActiveProject}>
+        <div className='projectCard' onClick={handleActiveProject}>
           <h3>Project 1</h3>
           <p>Project 1 description</p>
         </div>
-        <div className='projectCard' onClick={handleActiveProject}>
+        <div className='projectCard activeProjectCard' onClick={handleActiveProject}>
           <h3>Project 2</h3>
           <p>Project 2 description</p>
         </div>
