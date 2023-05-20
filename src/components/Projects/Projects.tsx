505ee78738465102490f66449d3f2a3de1dfc849
nadocodes <naderantar96@gmail.com>
2023-05-20 16:54:32 +0100

    updated projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,48 +1,61 @@
 import React from 'react';
+import { GoRepo } from 'react-icons/go';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import PushPlayListen from '../../assets/pushplaylistenScreenshot.jpeg';
 import './Projects.css';
 
 function Projects() {
   
   // Handle click on project card
   const handleActiveProject = (e: any) => {
+    // Get clicked project card
+    const projectCard = e.currentTarget.closest('.projectCard');
+    // If div is not a project card, return
+    if (!projectCard) {
+      return;
+    }
     // Remove activeProjectCard class from current active project
     const activeProject = document.querySelector('.activeProjectCard');
     if (activeProject) {
       activeProject.classList.remove('activeProjectCard');
     }
     // Add activeProjectCard class to clicked project card
-    if (e.target.classList.contains('projectCard')) {
-      e.target.classList.add('activeProjectCard');
-    } else {
-      e.target.parentElement.classList.add('activeProjectCard');
-    }
-    // Do nothing if clicked project card is already active
-    if (e.target.parentElement.classList.contains('activeProjectCard')) {
-      return;
-    }
+    projectCard.classList.add('activeProjectCard');
   }
 
   return (
     <div className='Projects primaryDefault' id='projects'>
-      <h2 className="projectsTitle">Projects</h2>
+      <h2 className="projectsTitle">Personal Projects</h2>
       <div className='projectsContainer'>
         <div className='projectCard' onClick={handleActiveProject}>
-          <img src='https://img.freepik.com/premium-photo/default-wallpaper-abstract-background-trendy-futuristic-wave-minimalistic-3d-layout-design_477306-800.jpg' alt='Project 1' className='projectCardImg' />
+          <img src={PushPlayListen} alt='Project 1' className='projectCardImg' />
           <h4>PushPlayListen</h4>
-          <p>Project 1 description</p>
+          <div>A responsive playlist app uses authentication Spotify's Api.</div>
+          <div className='projectButtons'>
+          <a className='projectCodeBtn projectLink' href='https://github.com/nadocodes/pushplaylisten-react-app.git' target='_blank'  ><GoRepo/></a>
+          <a className='projectDemoBtn projectLink' href='https://pushplaylisten.netlify.app/' target='_blank' >Live View <FaExternalLinkAlt/></a>
+          </div>
         </div>
         <div className='projectCard activeProjectCard' onClick={handleActiveProject}>
           <img src='https://img.freepik.com/premium-photo/default-wallpaper-abstract-background-trendy-futuristic-wave-minimalistic-3d-layout-design_477306-800.jpg' alt='Project 2' className='projectCardImg' />
           <h4>Portfolio 2023</h4>
-          <p>Project 2 description</p>
+          <div>Portfolio coded from scratch to showcase personal profile.</div>
+          <div className='projectButtons'>
+          <a className='projectCodeBtn projectLink' href='https://github.com/nadocodes/nader-antar.git' target='_blank'  ><GoRepo/></a>
+          <a className='projectDemoBtn projectLink' href='/'>Live View <FaExternalLinkAlt/></a>
+          </div>
         </div>
         <div className='projectCard' onClick={handleActiveProject}>
           <img src='https://img.freepik.com/premium-photo/default-wallpaper-abstract-background-trendy-futuristic-wave-minimalistic-3d-layout-design_477306-800.jpg' alt='Project 3' className='projectCardImg' />
           <h4>Extra Space</h4>
           <p>Project 3 description</p>
+          <div className='projectButtons'>
+          <a className='projectCodeBtn projectLink' href='https://pushplaylisten.netlify.app' target='_blank'  ><GoRepo/></a>
+          <a className='projectDemoBtn projectLink' href='https://pushplaylisten.netlify.app/' target='_blank' >Live View <FaExternalLinkAlt/></a>
+          </div>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
