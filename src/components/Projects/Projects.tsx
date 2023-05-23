ac6b0653ea3a0b38d84cf7e31009477f14cae8d3
nadocodes <naderantar96@gmail.com>
2023-05-23 02:13:24 +0100

    updated margins for better screen response

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,28 +7,28 @@ import './Projects.css';
 
 function Projects() {
   
-  // Handle click on project card
-  const handleActiveProject = (e: any) => {
-    // Get clicked project card
-    const projectCard = e.currentTarget.closest('.projectCard');
-    // If div is not a project card, return
-    if (!projectCard) {
-      return;
-    }
-    // Remove activeProjectCard class from current active project
-    const activeProject = document.querySelector('.activeProjectCard');
-    if (activeProject) {
-      activeProject.classList.remove('activeProjectCard');
-    }
-    // Add activeProjectCard class to clicked project card
-    projectCard.classList.add('activeProjectCard');
-  }
+  // // Handle click on project card
+  // const handleActiveProject = (e: any) => {
+  //   // Get clicked project card
+  //   const projectCard = e.currentTarget.closest('.projectCard');
+  //   // If div is not a project card, return
+  //   if (!projectCard) {
+  //     return;
+  //   }
+  //   // Remove activeProjectCard class from current active project
+  //   const activeProject = document.querySelector('.activeProjectCard');
+  //   if (activeProject) {
+  //     activeProject.classList.remove('activeProjectCard');
+  //   }
+  //   // Add activeProjectCard class to clicked project card
+  //   projectCard.classList.add('activeProjectCard');
+  // }
 
   return (
     <div className='Projects primaryDefault' id='projects'>
       <h2 className="projectsTitle">Personal Projects</h2>
       <div className='projectsContainer'>
-        <div className='projectCard' onClick={handleActiveProject}>
+        <div className='projectCard'>
           <img src={PushPlayListen} alt='Project 1' className='projectCardImg' />
           <h4>PushPlayListen</h4>
           <p>A responsive playlist app uses authentication Spotify's Api.</p>
@@ -37,7 +37,7 @@ function Projects() {
             <a className='projectDemoBtn projectLink' href='https://pushplaylisten.netlify.app/' target='_blank' >Live View <FaExternalLinkAlt/></a>
           </div>
         </div>
-        <div className='projectCard activeProjectCard' onClick={handleActiveProject}>
+        <div className='projectCard activeProjectCard'>
           <img src={Portfolio2023} alt='Project 2' className='projectCardImg' />
           <h4>Portfolio 2023</h4>
           <p>Portfolio coded from scratch to showcase personal profile.</p>
@@ -45,7 +45,7 @@ function Projects() {
             <a className='projectDemoBtn projectLink' href='https://github.com/nadocodes/nader-antar.git' target='_blank'  ><GoRepo/></a>
           </div>
         </div>
-        <div className='projectCard' onClick={handleActiveProject}>
+        <div className='projectCard'>
           <img src='https://img.freepik.com/premium-photo/default-wallpaper-abstract-background-trendy-futuristic-wave-minimalistic-3d-layout-design_477306-800.jpg' alt='Project 3' className='projectCardImg' />
           <h4>Extra Space</h4>
           <p>Project 3 description</p>
